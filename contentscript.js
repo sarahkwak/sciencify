012d7e0ba16a52d8579cefb1f4a4962b6d2de3d7
miniyakkos <ayakokurushima@gmail.com>
2015-10-24 17:59:33 -0700

    logic to block websites with bad words

--- contentscript.js
+++ contentscript.js
@@ -1,4 +1,13 @@
 var badsite_goaway = (function() {
+var taboo = ["foxnews", "trump"];
+  function checker() {
+    for (var i = 0; i <= taboo.length; i++) {
+      if (window.location.href.includes(taboo[i])) {
+        eraser();
+        redirect();
+      }
+    }
+  };
   function eraser() {
     document.body.style.display = "none";
     alert("Unscientific page detected!")
@@ -7,10 +16,11 @@ var badsite_goaway = (function() {
     location.replace("http://www.sciencemag.org/")
   };
   return {
+    checker: checker,
     eraser: eraser,
     redirect: redirect
   }
 })();
-
-badsite_goaway.eraser();
-badsite_goaway.redirect();
+badsite_goaway.checker();
+// badsite_goaway.eraser();
+// badsite_goaway.redirect();
